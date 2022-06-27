5027edb45fad88fbef4749a43327418b72706b30
legit_123 <legitgamer2345@gmail.com>
2022-06-27 16:30:54 +0530

    Updated UI

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React, { useState } from 'react';
+import questions from './data';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return(
-      <section className="container">
-        <h3>Questions And Answers About Login</h3>
-        <section>
-          {data.map((question)=>(
-            <SingleQuestion {...question}></SingleQuestion>
-          ))}
-        </section>
+  const[question,setQuestions] = useState(data);
+  return <main>
+    <div className="container">
+      <h3>questions and answers about login</h3>
+      <section className="info">
+        {
+          questions.map((question)=>{
+            return <SingleQuestion key = {question.id} {...question}></SingleQuestion>
+          })
+        }
       </section>
-  )
+    </div>
+  </main>
+  // return(
+      // <section className="container">
+      //   <h3>Questions And Answers About Login</h3>
+      //   <section>
+      //     {data.map((question)=>(
+      //       <SingleQuestion {...question}></SingleQuestion>
+      //     ))}
+      //   </section>
+      // </section>
+  // )
 }
 
 export default App;
--- src/Question.js
+++ src/Question.js
@@ -1,16 +1,31 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = ({id,title,info}) => {
-  const[btnFlag,setBtnFlag] = useState(false);
-  return(
-    <section className="question">
-      <div className="header">
-       <h4>{title}</h4>
-          {btnFlag===false?<AiOutlinePlus className ="btn" onClick = {()=>setBtnFlag(!btnFlag)}></AiOutlinePlus>:<AiOutlineMinus className="btn" onClick={()=>setBtnFlag(!btnFlag)}></AiOutlineMinus>}
-      </div>
-      <p>{btnFlag===true?info:''}</p>
-    </section>
-  )
-};
+
+const Question = ({title,info}) => {
+   const[showInfo,setShowInfo] = useState(false);
+   return (
+   <article className="question">
+    <header>
+      <h4>{title}</h4>
+      <button className="btn" onClick = {()=>setShowInfo(!showInfo)}>
+       {showInfo?<AiOutlineMinus></AiOutlineMinus>:<AiOutlinePlus></AiOutlinePlus>}
+      </button>
+    </header>
+    {showInfo && <p>{info}</p>}
+   </article>
+   )
+}
+// const Question = ({id,title,info}) => {
+//   const[btnFlag,setBtnFlag] = useState(false);
+//   return(
+//     <section className="question">
+//       <div className="header">
+//        <h4>{title}</h4>
+//           {btnFlag===false?<AiOutlinePlus className ="btn" onClick = {()=>setBtnFlag(!btnFlag)}></AiOutlinePlus>:<AiOutlineMinus className="btn" onClick={()=>setBtnFlag(!btnFlag)}></AiOutlineMinus>}
+//       </div>
+//       <p>{btnFlag===true?info:''}</p>
+//     </section>
+//   )
+// };
 
 export default Question;
